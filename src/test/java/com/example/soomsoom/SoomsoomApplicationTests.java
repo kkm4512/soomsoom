36d8f7a7e65c560cb8d97998d286b87e6446cd8d
kkm4512 <nayoun440@naver.com>
2024-12-18 16:29:18 +0900

    init

---
+++ src/test/java/com/example/soomsoom/SoomsoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soomsoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoomsoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
